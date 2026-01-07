74bda12d3caff6a04b509a2ce9a5f8f88474366b
Aman Singh <amandgb2005@gmail.com>
2026-01-07 08:40:03 +0530

    comleted

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,11 @@ import sys
 
 import numpy as np
 import pandas as pd
-from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+from src.exception import CustomException
+
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -15,6 +16,7 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -39,6 +41,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
